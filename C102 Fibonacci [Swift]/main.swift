d5b1b336bcd893a3f1277450a5ce18f9629ce0a7
sri-nagasai-siva-paladugu <srinagasaisiva.paladugu@student.allenisd.org>
2022-11-14 10:40:42 -0600

    Followed Best Coding Practices for C102

--- C102 Fibonacci [Swift]/main.swift
+++ C102 Fibonacci [Swift]/main.swift
@@ -9,20 +9,23 @@ let n = 7
 
 // Add your code below:
 
+/*
+Recursive functions have essentially two sections: the base case and the recursive step.
+ */
 
 
-func fibonacci(_ n: Int) -> Int {
-    print("Calculating fibonacci" + "(" + "\(n)" + ")...")
-    if (n <= 2) {
+func fibonacci(_ n: Int) -> Int { // Start of the fibonacci function
+    print("Calculating fibonacci" + "(" + "\(n)" + ")...") // Prints using string interpoleration
+    if (n <= 2) { // Values less than or equal 2 will result in a terminal case
         print("Terminal case, returning 1.")
-        return 1
+        return 1 // Sends a value of 1 back to the function //    // Base case //
     }
-    else {
-        let  result = fibonacci(n-1) + fibonacci(n-2)
-        print("Non-terminal case, returning \(result).")
-        return result
+    else { // If it failes the primary condition specified above, it will run the conditions below 
+        let  result = fibonacci(n-1) + fibonacci(n-2) // Fibonacci formula // 
+        print("Non-terminal case, returning \(result).") // Prints using string interpoleration 
+        return result // Sends a value inside the result back to the function   // Recursive step //
     }
-
+    
 }
 print("The final result is: \(fibonacci(n)).")
 

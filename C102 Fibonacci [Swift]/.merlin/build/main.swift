6585dee3c02f4d5070480999b88f9e6cdbc79b40
sri-nagasai-siva-paladugu <srinagasaisiva.paladugu@student.allenisd.org>
2022-11-14 09:55:35 -0600

    Added all the mission files

---
+++ C102 Fibonacci [Swift]/.merlin/build/main.swift
@@ -0,0 +1,28 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 1
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+
+
+func fibonacci(_ n: Int) -> Int {
+    print("Calculating fibonacci" + "(" + "\(n)" + ")...")
+    if (n <= 2) {
+        print("Terminal case, returning 1.")
+        return 1
+    }
+    else {
+        let  result = fibonacci(n-1) + fibonacci(n-2)
+        print("Non-terminal case, returning \(result).")
+        return result
+    }
+
+}
+print("The final result is: \(fibonacci(n)).")
+

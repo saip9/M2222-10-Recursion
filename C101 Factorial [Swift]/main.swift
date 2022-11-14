8f8c17428cc7a072fe5daf96b1cc97bd3d4a0aca
sri-nagasai-siva-paladugu <srinagasaisiva.paladugu@student.allenisd.org>
2022-11-14 10:24:54 -0600

    Followed the Best Coding Practices

--- C101 Factorial [Swift]/main.swift
+++ C101 Factorial [Swift]/main.swift
@@ -9,18 +9,18 @@ let n = 7
 
 // Add your code below:
 
-func factorial(_ n: Int) -> Int {
-         print("Calculating factorial" + "(" + "\(n)" + ")...")
-    if (n <= 1) {
-    print("Terminal case, returning 1.")
-    return 1
-}
-else {
- let  result = n * factorial(n-1)
- print("Non-terminal case, returning \(result).")
-return result
-}
-
+func factorial(_ n: Int) -> Int { // Start of the factorial function
+    print("Calculating factorial" + "(" + "\(n)" + ")...") // Prints the first message according to the instructions using string interpoleration
+    if (n <= 1) { // As soon as the the value of n descreases to 1, it runs the condition
+        print("Terminal case, returning 1.")
+        return 1 // Sends a value of 1 back to the function
+    }
+    else {
+        let  result = n * factorial(n-1)  // Formula for calculating factorial 
+        print("Non-terminal case, returning \(result).")
+        return result // Sends a value inside result back to the function
+    }
+    
 }
 print("The final result is: \(factorial(n)).")
 
